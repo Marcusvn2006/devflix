23fb8fc0f5046f0fd21a652f1cfb2fc12fd623b7
marcusdograu088 <zubuff@gmail.com>
2023-08-18 17:01:12 -0300

    finalizamos o movieDescription.module.css movieDescription.jsx tbm

--- src/componentes/movieCard/movieCard.jsx
+++ src/componentes/movieCard/movieCard.jsx
@@ -1,9 +1,16 @@
+import { useState } from "react";
+import MovieDesc from "../movieDescription/movieDescription";
 import styles from "./movieCard.module.css";
 
 const MovieCard = ({ movies }) => {
   const movie = movies;
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const toggleModal =() =>{
+    setIsModalOpen(!isModalOpen)
+  };
   return (
-    <div className={styles.movie}>
+<>
+<div className={styles.movie} onClick={toggleModal}>
       <div>
         <p>{movie.Year}</p>
       </div>
@@ -17,6 +24,9 @@ const MovieCard = ({ movies }) => {
 <h3>{movie.Title}</h3>
          </div>
     </div>
+  
+{isModalOpen && <MovieDesc movies={movie} click={toggleModal} />}
+  </>
   );
 };
 
